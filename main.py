49cb82beedbe33aeab585e243450425798cba17b
Jami Islam Anik <anik.islam1494@gmail.com>
2023-07-07 18:20:18 +0600

    Added crossmiddleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response
 from fastapi.testclient import TestClient
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import blog, auth
 from database import Base, engine
@@ -10,7 +11,18 @@ from database import Base, engine
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Assuming your frontend runs on port 3000
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 
 
